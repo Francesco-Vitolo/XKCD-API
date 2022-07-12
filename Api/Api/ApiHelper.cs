dc15a5464a1e307dfa8cb2137c781ff4715db3ad
Francesco-Vitolo <94391020+Francesco-Vitolo@users.noreply.github.com>
2022-07-12 21:33:43 +0200

    Add files via upload

---
+++ Api/Api/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Api
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void IntizializeClient()
+        {
+            ApiClient = new HttpClient();
+            //ApiClient.BaseAddress = new Uri("http://xkcd.com/");
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Api/GUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Api;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int currentNumber;
+        Random rand = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.IntizializeClient();
+        }
+
+        private async Task LoadImg(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+            currentNumber = comic.Num;
+            var uriSrc = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSrc);
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImg();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+           int number = rand.Next(0, 201);
+            await LoadImg(number);
+        }
+    }
+}
